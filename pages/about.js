37b49e4f3e5c7a53bcda334c968195622c250bc5
yarelleonfitness <yarelleonfitness@gmail.com>
2025-12-23 21:07:44 -0600

    Create about.js

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div className="container">
+      <h1>About YL Fitness</h1>
+      <p>
+        Yarel Leon (YL) Fitness Training offers personalized coaching for both athletic and non-athletic clients. 
+        Our programs are designed to transform your health and lifestyle.
+      </p>
+    </div>
+  );
+}
