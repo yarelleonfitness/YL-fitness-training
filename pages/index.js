a8e3333da4535c280a515968500c33df61cc3490
yarelleonfitness <yarelleonfitness@gmail.com>
2025-12-23 23:36:06 -0600

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="cta-section">
-      <h1>Welcome to YL Fitness Training</h1>
-      <p>Your journey to strength, health, and confidence starts here.</p>
-      <button>Book Free Consultation</button>
-    </div>
-  );
-}
