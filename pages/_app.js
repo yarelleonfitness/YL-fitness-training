2d1e9682e0c6c7e3d9fae12159eb00ff56efbbd6
yarelleonfitness <yarelleonfitness@gmail.com>
2025-12-23 21:05:07 -0600

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import '../styles/globals.css';
+import Navbar from '../components/Navbar';
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
+}
