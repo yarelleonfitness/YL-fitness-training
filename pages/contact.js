1309e3795bdc66df934fcad95e8917bc0e076c31
yarelleonfitness <yarelleonfitness@gmail.com>
2025-12-23 21:11:25 -0600

    Create contact.js

---
+++ pages/contact.js
@@ -0,0 +1,10 @@
+export default function Contact() {
+  return (
+    <div className="container">
+      <h1>Contact Us</h1>
+      <p>Email: info@ylfitnesstraining.com</p>
+      <p>Phone: (123) 456-7890</p>
+      <button>Book a Free Consultation</button>
+    </div>
+  );
+}
